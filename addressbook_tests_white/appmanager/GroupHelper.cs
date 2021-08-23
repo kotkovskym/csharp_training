c9374ef84bdf9584f67eff6098134846305c11ac
Mariia <mariia.kotkova@gmail.com>
2021-08-23 22:48:12 +0300

    Add autoIt and white projects

---
+++ addressbook_tests_white/appmanager/GroupHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.WindowItems;
+
+
+namespace addressbook_tests_white
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            dialogue.Get<Tree>("");
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetText", "#0|#"+i, "");
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            }
+            CloseGroupsDialogue(dialogue);
+            return list;
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            dialogue.Get<Button>("").Click();
+            
+            //aux.Send(newGroup.Name);
+            //aux.Send("{ENTER}");
+            CloseGroupsDialogue(dialogue);
+        }
+
+        private void CloseGroupsDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("").Click();
+        }
+
+        private Window OpenGroupsDialogue()
+        {
+            manager.MainWindow.Get<Button>("").Click();
+            return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+    }
+}
